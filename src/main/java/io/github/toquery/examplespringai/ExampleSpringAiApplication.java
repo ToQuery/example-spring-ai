d19d8f6f48fc7217d46b9961b21e5f4581353f3e
ToQuery <toquery@qq.com>
2024-03-13 10:51:24 +0800

    first commit

---
+++ src/main/java/io/github/toquery/examplespringai/ExampleSpringAiApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/examplespringai/ExampleSpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
