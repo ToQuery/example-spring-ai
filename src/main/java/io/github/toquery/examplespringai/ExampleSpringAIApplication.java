cfa59851aff930cd2400393f6d415ab6cc128dac
ToQuery <toquery@qq.com>
2024-03-13 11:48:58 +0800

    bug

--- src/main/java/io/github/toquery/examplespringai/ExampleSpringAIApplication.java
+++ src/main/java/io/github/toquery/examplespringai/ExampleSpringAIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleSpringAiApplication {
+public class ExampleSpringAIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleSpringAiApplication.class, args);
+		SpringApplication.run(ExampleSpringAIApplication.class, args);
 	}
 
 }
